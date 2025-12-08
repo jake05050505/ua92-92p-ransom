584c058d3dd112b915af3998d1127b4f60db5fa6
jake05050505 <jakehancox12@gmail.com>
2025-12-08 17:34:24 +0000

    change encryption handling, add support for multi-line files, add error handling
    encryption is no longer handled using strings, the files are now opened and written to in bytes mode - reading and writing bytes directly, instead of str.encode() and str.decode(). Instead of only reading the first line of files, all files are read as a single bytes object.

--- app.py
+++ app.py
@@ -1,47 +1,79 @@
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet,InvalidToken
 import random
 import os
+import sys
 
 with open("key.key", 'rb') as kf:
     key = kf.read()
-
-    # print(key)
 
 fn = Fernet(key)
 
 os.chdir("encrypt_test")
 
-def encrypt_random():
-    dir = os.listdir()
-    ran = random.randint(0,len(dir)-1)
-    random_file = dir[ran]
+def encrypt_random() -> bool:
+    files = os.listdir()
+    files = [file for file in files if os.path.isfile(file)]
+    print(files)
+    random_index = random.randint(0,len(files)-1)
+    file = files[random_index]
 
-    with open(random_file, 'r') as f:
-        content = f.readlines()[0].encode()
-    encrypted_content = str(fn.encrypt(content))
+    with open(file, 'rb') as f:
+        content = f.read()
 
-    with open(random_file, 'w') as f:
+    try:
+        fn.decrypt(content)
+        print(f"File: {file} already encrypted!")
+        return False
+    except InvalidToken:
+        pass
+    except Exception as e:
+        raise(e)
+
+    encrypted_content = fn.encrypt(content)
+
+    with open(file, 'wb') as f:
         f.write(encrypted_content)
 
-    print(f"File {random_file} encrypted!")
+    print(f"File: {file} encrypted!")
+    return True
 
 def decrypt_all():
-    for file in os.listdir():
-        with open(file, 'r') as f:
-            content = f.readlines()[0]
-            if content[0:2] == 'b\'' and content[-1] == '\'':
-                decrypted_content = fn.decrypt(content.lstrip('b').strip('\'').encode())
-                pass
-            else:
-                decrypted_content = None
+    files = os.listdir()
+    files = [file for file in files if os.path.isfile(file)]
+
+    for file in files:
+        with open(file, 'rb') as f:
+            content = f.read()
+
+        try:
+            decrypted_content = fn.decrypt(content)
+            print(f"Decrypted file: {file}")
+        except InvalidToken:
+            decrypted_content = None
+        except Exception as e:
+            raise(e)
 
         if decrypted_content:
-            with open(file, 'w') as f:
-                f.write(str(decrypted_content).lstrip('b').strip('\''))
+            with open(file, 'wb') as f:
+                f.write(decrypted_content)
+
+register = {
+    "1":encrypt_random,
+    "2":decrypt_all,
+}
+
+def main():
+    try:
+        choice = input("1 = encrypt random\n2 = decrypt all\n$ ")
+        func = register.get(choice)
+        if func:
+            func()
+        else:
+            print(f"Unknown option: {choice}")
+    except KeyboardInterrupt:
+        print("\nStopping...")
+        sys.exit(2)
+
 
 if __name__ == "__main__":
-    choice = int(input("1 = encrypt random\n2 = decrypt all\n$ "))
-    if choice == 1:
-        encrypt_random()
-    elif choice == 2:
-        decrypt_all()
+    main()
